b1f01ebef10c47ebd9d3976651b1d4f47cbf931c
ZhangKe <1129987654@qq.comm>
2023-06-27 16:11:07 +0800

    初版

--- hzapt-admin/src/main/java/com/hzapt/admin/AdminStarter.java
+++ hzapt-admin/src/main/java/com/hzapt/admin/AdminStarter.java
@@ -3,15 +3,11 @@ package com.hzapt.admin;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 
 
 @Slf4j
@@ -22,20 +18,8 @@ import java.net.UnknownHostException;
 @EnableAspectJAutoProxy // 开启AOP代理
 public class AdminStarter {
 
-    public static void main(String[] args)  throws UnknownHostException {
-        ConfigurableApplicationContext application = SpringApplication.run(AdminStarter.class, args);
-        Environment env = application.getEnvironment();
-        log.info("\n----------------------------------------------------------\n\t" +
-                        "Application '{}' is running! Access URLs:\n\t" +
-                        "Local: \t\thttp://localhost:{}/doc.html\n\t" +
-                        "bootstrapUI: \thttp://{}:{}/doc.html\n" +
-                        "----------------------------------------------------------",
-                env.getProperty("spring.application.name"),
-                env.getProperty("server.port"),
-                InetAddress.getLocalHost().getHostAddress(),
-                env.getProperty("server.port"),
-                InetAddress.getLocalHost().getHostAddress(),
-                env.getProperty("server.port"));
+    public static void main(String[] args) {
+        SpringApplication.run(AdminStarter.class, args);
         log.info("##################	hzapt-admin  SUCCESS");
     }
 
